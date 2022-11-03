ef6146e2ceb3c30fdc96c4c5b3e1301b2bd21eef
asb1512 <andrew@olympicmedia.com>
2022-11-03 17:05:35 -0400

    rendering 3 colors

--- src/components/ColorContainer.jsx
+++ src/components/ColorContainer.jsx
@@ -1,21 +1,64 @@
-import React, { useState } from 'react';
-import generateHexValue from '../services/generateHexValue';
+import React, { useState, useEffect } from 'react';
+import { useTrail, animated } from '@react-spring/web';
+import generateCorrectColor from '../services/generateCorrectColor';
+import generateHexSet from '../services/generateHexSet';
 
 export default function ColorContainer() {
-  const [data, setData] = useState('000000');
+  const [hexData, setHexData] = useState(generateHexSet());
+  const [correctColor, setCorrectColor] = useState(generateCorrectColor());
+  const [active, setActive] = useState(false);
+
+  const verifyClick = (hex) => {
+    if (hexData[correctColor] === hex) {
+      setActive(false);
+      setTimeout(() => {
+        setHexData(generateHexSet());
+        setCorrectColor(generateCorrectColor());
+      }, 750);
+    }
+  };
+
+  const verifyKeyDown = (e, hex) => {
+    if (e.key === 'Enter') {
+      verifyClick(hex);
+    }
+  };
+
+  const trails = useTrail(3, {
+    height: active ? '100%' : '0',
+    opacity: active ? 1 : 0,
+    onRest: () => setActive(true),
+  });
+
+  const renderColors = hexData.map((hex, idx) => (
+    <animated.div
+      key={hex}
+      role="option"
+      tabIndex={idx}
+      className="color"
+      aria-label={`color option #${idx + 1}`}
+      aria-selected
+      style={{
+        backgroundColor: `#${hex}`,
+        ...trails[idx],
+      }}
+      onClick={() => verifyClick(hex)}
+      onKeyDown={(e) => verifyKeyDown(e, hex)}
+    />
+  ));
+
+  useEffect(() => {
+    setActive(true);
+  }, []);
 
   return (
     <>
-      <div className="color-cntr" style={{ backgroundColor: `#${data}` }}>
-        <div>#{data}</div>
+      <div className="color-cntr">
+        {renderColors}
       </div>
-      <button
-        type="button"
-        className="color-btn"
-        onClick={() => setData(generateHexValue())}
-      >
-        Generate Color
-      </button>
+      <div className="hex-cntr">
+        <div className="hex">#{hexData[correctColor]}</div>
+      </div>
     </>
   );
 }
