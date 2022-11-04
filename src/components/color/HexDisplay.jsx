33e63c6d037145b876a60ffa87ec68830251b397
asb1512 <andrew@olympicmedia.com>
2022-11-04 11:33:15 -0400

    animating progress bar

--- src/components/color/HexDisplay.jsx
+++ src/components/color/HexDisplay.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSpring, animated } from '@react-spring/web';
 
 export default function HexDisplay({
   hexData,
   correctColor,
+  currentRound,
 }) {
+  const progressSpring = useSpring({
+    width: currentRound.correct ? '100%' : `${currentRound.incorrect * 33}%`,
+    backgroundColor: currentRound.correct ? 'green' : 'red',
+  });
+
   return (
     <div className="hex-cntr">
       <div className="hex">#{hexData[correctColor]}</div>
-      <div className="progress-tracker" />
+      <animated.div className="progress-tracker" style={progressSpring} />
     </div>
   );
 }
@@ -16,4 +23,8 @@ export default function HexDisplay({
 HexDisplay.propTypes = {
   hexData: PropTypes.arrayOf(PropTypes.string).isRequired,
   correctColor: PropTypes.number.isRequired,
+  currentRound: PropTypes.shape({
+    correct: PropTypes.bool.isRequired,
+    incorrect: PropTypes.number.isRequired,
+  }).isRequired,
 };
