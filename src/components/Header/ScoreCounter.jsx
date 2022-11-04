f2cd03db1391c2f7bf5e15763f5235f432ec9ab2
asb1512 <andrew@olympicmedia.com>
2022-11-04 17:41:23 -0400

    populating with score from context

--- src/components/Header/ScoreCounter.jsx
+++ src/components/Header/ScoreCounter.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useScore } from '../../context/ScoreProvider';
 
 export default function ScoreCounter() {
+  const { state: { score } } = useScore();
+
   return (
     <div className="score-cntr">
       <div className="score-title">Score</div>
-      <div className="score">300</div>
+      <div className="score">{score}</div>
     </div>
   );
 }
