9e21a3e4ab2ac538c3daf2dcdc72a681d0eb6100
asb1512 <andrew@olympicmedia.com>
2022-11-04 16:22:06 -0400

    score coutner for header

---
+++ src/components/Header/ScoreCounter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ScoreCounter() {
+  return (
+    <div className="score-cntr">
+      <div className="score-title">Score</div>
+      <div className="score">300</div>
+    </div>
+  );
+}
