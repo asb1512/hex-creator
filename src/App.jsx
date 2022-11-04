2a61b55b59a779bcaeaa3624945dccb55ebca2f0
asb1512 <andrew@olympicmedia.com>
2022-11-04 14:16:53 -0400

    rendering header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import ColorContainer from './components/color/ColorContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ColorContainer />
     </div>
   );
