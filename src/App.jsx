e2048ec05be04e9fadcd8486281a33424d9f7ab4
asb1512 <andrew@olympicmedia.com>
2022-11-07 13:01:20 -0500

    setting device type; moved context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import { ScoreProvider } from './context/ScoreProvider';
+import React, { useEffect, useState } from 'react';
+import { useAppContext } from './context/AppContext';
 import Header from './components/Header/Header';
 import ColorContainer from './components/color/ColorContainer';
 import './App.css';
 
 function App() {
+  const { dispatch } = useAppContext();
+  const [appWidth, setAppWidth] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  const detectAppSize = () => {
+    setAppWidth({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', detectAppSize);
+    if (appWidth.winWidth <= 750) {
+      dispatch({ type: 'setDeviceType', payload: true });
+    } else {
+      dispatch({ type: 'setDeviceType', payload: false });
+    }
+
+    return () => window.removeEventListener('resize', detectAppSize);
+  }, [appWidth]);
+
   return (
     <div className="App">
-      <ScoreProvider>
-        <Header />
-        <ColorContainer />
-      </ScoreProvider>
+      <Header />
+      <ColorContainer />
     </div>
   );
 }
