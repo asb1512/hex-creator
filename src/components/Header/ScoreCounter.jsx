5ebc018e73b38d7de5abbf246043f674fd0d7403
asb1512 <andrew@olympicmedia.com>
2022-11-07 09:52:14 -0500

    animating the score from previous state to current

--- src/components/Header/ScoreCounter.jsx
+++ src/components/Header/ScoreCounter.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useCountUp } from 'react-countup';
 import { useScore } from '../../context/ScoreProvider';
 
 export default function ScoreCounter() {
-  const { state: { score } } = useScore();
+  const { state: { score, prevScore } } = useScore();
+
+  const scoreRef = useRef(null);
+  const { update } = useCountUp({
+    ref: scoreRef,
+    start: prevScore,
+    end: score,
+    duration: 1,
+  });
+
+  useEffect(() => {
+    update(score);
+  }, [score]);
 
   return (
     <div className="score-cntr">
       <div className="score-title">Score</div>
-      <div className="score">{score}</div>
+      <div ref={scoreRef} className="score">
+        {score}
+      </div>
     </div>
   );
 }
