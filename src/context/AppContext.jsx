99ff9fe482ca9b3fbc8d9a1e2fea9df8b3d6d691
asb1512 <andrew@olympicmedia.com>
2022-11-08 15:29:15 -0500

    added round counter to app state

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -8,26 +8,36 @@ import PropTypes from 'prop-types';
 
 const AppContext = createContext();
 
-function scoreReducer(state, action) {
+function contextReducer(state, action) {
   switch (action.type) {
     case 'setDeviceType':
       return {
         ...state,
         mobile: action.payload,
       };
+    case 'incrementRound':
+      return {
+        ...state,
+        round: state.round + 1,
+      };
     case 'addTwentyFive':
       return {
+        round: state.round,
         mobile: state.mobile,
         score: state.score + 25,
         prevScore: state.score,
       };
     case 'addFifty':
       return {
+        round: state.round,
+        mobile: state.mobile,
         score: state.score + 50,
         prevScore: state.score,
       };
     case 'addOneHundred':
       return {
+        round: state.round,
+        mobile: state.mobile,
         score: state.score + 100,
         prevScore: state.score,
       };
@@ -37,7 +47,8 @@ function scoreReducer(state, action) {
 }
 
 function ContextProvider({ children }) {
-  const [state, dispatch] = useReducer(scoreReducer, {
+  const [state, dispatch] = useReducer(contextReducer, {
+    round: 1,
     score: 0,
     prevScore: 0,
     mobile: false,
