a09029b29e1dc46f3f62fc2faebb28764ca3ef27
asb1512 <andrew@olympicmedia.com>
2022-11-08 18:38:52 -0500

    start btn can reset game state

--- src/components/Welcome/StartButton.jsx
+++ src/components/Welcome/StartButton.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useAppContext } from '../../context/AppContext';
 import './StartButton.css';
 
 function StartButton({ setGameStatus }) {
+  const { dispatch } = useAppContext();
+
+  const handleClick = () => {
+    // user's first game
+    if (setGameStatus) {
+      setGameStatus(true);
+    } else {
+      dispatch({ type: 'restartGame' });
+    }
+  };
+
   return (
     <button
       type="button"
       className="new-game-btn"
-      onClick={() => setGameStatus(true)}
+      onClick={() => handleClick()}
     >
       New Game
     </button>
